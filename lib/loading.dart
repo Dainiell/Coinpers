87cb5dd10065dd08350eb3c5d0b91ca94407850f
Ralph Gonzaga <gonzagaralphdainiell@gmail.com>
2026-09-13 07:50:28 +0800

    New Commit

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
